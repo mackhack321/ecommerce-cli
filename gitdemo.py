316f2af617e68023b890ce19d87194f4c4758f3b
Mack Stanley <mlgprohacker@gmail.com>
2022-03-25 16:08:04 -0500

    add git demo file

---
+++ gitdemo.py
@@ -0,0 +1,17 @@
+def greetme(name):
+    print("\nHello, " + name + "!")
+
+
+print("Welcome to my greeter program.")
+name = input("What is your name? ")
+greetme(name)
+
+# git demo
+# - initial config name/email
+# - show how to stage and commit
+# - show what happens when you stage and then keep editing
+# - show how to undo commit
+# - show how to push
+# - show how to pull (sync button at bottom)
+# - commit message best practices
+# - more small commits > one big commit
