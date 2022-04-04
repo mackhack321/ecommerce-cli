69360720e22da235cbeee943c9f2b399127a8737
Mack Stanley <mlgprohacker@gmail.com>
2022-04-04 14:21:56 -0500

    split up files

---
+++ dbOpenConn.py
@@ -0,0 +1,13 @@
+import mysql.connector as sql
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def openDBConnection():
+    return sql.connect(
+        user=getenv('USERNAME'),
+        password=getenv('PASS'),
+        host=getenv('HOST'),
+        database=getenv('DB')
+    )
