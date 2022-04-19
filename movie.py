1bc435c5f104612d6e1ba100a98cf7e1c7be3cfd
Jade Parker <jadeparker14@yahoo.com>
2022-04-19 18:38:34 -0500

    initalizing movie

--- movie.py
+++ movie.py
@@ -2,4 +2,14 @@
 from dbOpenConn import openDBConnection
 
 class Movie:
-    ...
+    def __init__(self, id, title, genre, director, rating, year, price, quantity):
+        self.movieID = id
+        self.title = title
+        self.genre = genre
+        self.director = director
+        self.rating = rating
+        self.year = year
+        self.price = price
+        self.quantity = quantity
+
+    
