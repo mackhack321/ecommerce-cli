ca256c838e9663911597a1876e2ea2c176192739
Philip Haggard <prh148@msstate.edu>
2022-04-22 14:17:07 -0500

    working on removeItemStock

--- inventory.py
+++ inventory.py
@@ -6,7 +6,24 @@ class Inventory:
         self.update()
     
     def removeItemStock(self, id, qty):
-        ...
+        db = openDBConnection()
+        cursor = db.cursor(dictionary=True)
+        success = True
+
+        query = f"UPDATE movies SET quantity=%s WHERE movieID=%s"
+        data = (qty, id)
+
+        try:
+            cursor.execute(query, data)
+            db.commit()
+        except:
+            success = False
+
+        cursor.close()
+        db.close()
+
+        return success
+
     
     def update(self):
         ...
