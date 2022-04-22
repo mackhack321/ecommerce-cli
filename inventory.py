54cc717b23841c1c6e862767d08080668cef5582
Mack Stanley <mlgprohacker@gmail.com>
2022-04-21 20:23:26 -0500

    add init and blank functions

--- inventory.py
+++ inventory.py
@@ -1,4 +1,12 @@
 from dbOpenConn import openDBConnection
 
 class Inventory:
-    ...
+    def __init__(self):
+        self.items = []
+        self.update()
+    
+    def removeItemStock(self, id, qty):
+        ...
+    
+    def update(self):
+        ...
