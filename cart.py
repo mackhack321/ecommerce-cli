37a08cd12c583eb5c297d087b4f33de287fe081a
Reyki <FlamingBeast20@hotmail.com>
2022-04-21 19:37:23 -0500

    creating an add function!:)

--- cart.py
+++ cart.py
@@ -1,5 +1,16 @@
+from operator import truediv
 from dbOpenConn import openDBConnection
+from datetime import date
 
 
 class Cart:
-    ...
+    def __init__(self):
+        pass
+
+    def addItem(self, userID, movieID, qty):
+        success = True
+        db = openDBConnection()
+        cursor = db.cursor(dictionary=True)
+
+        query = "INSERT INTO cart(userID, movieID, quantity) VALUES (%s, %s, %s,)"
+        data = (self.userID, movieID, qty, date.today().strftime("%m/%d/%Y"))
